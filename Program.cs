d732d7ac3c3a6a3f3eef53b5f8b03719762988ec
Djack <laktozar@mail.ru>
2023-12-06 20:44:54 +0300

    programs

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 1000) {
+    int result = num / 100 * 10 + 1;
+    Console.WriteLine($"Полученное число {result}");
+} else {
+    Console.WriteLine("Некорректное число");
+}
