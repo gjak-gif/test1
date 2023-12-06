6342007823d7a7fd58a090dc9fad996925008b4a
Djack <laktozar@mail.ru>
2023-12-06 21:55:20 +0300

    prograns

--- Program.cs
+++ Program.cs
@@ -1,9 +1,62 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
-if (num > 99 && num < 1000) {
-    int result = num / 100 * 10 + 1;
-    Console.WriteLine($"Полученное число {result}");
-} else {
-    Console.WriteLine("Некорректное число");
+﻿/ написать программу, которая на вход принемает 2 целых числа и
+// проверяет являються ли первые числа квадратом второго 
+//a = 25, b = 5 => yes
+//a = 2, b = 10 => no
+//a = 9, b = -3 => yes
+//a = -3, b = 9 -> no
+//получения 2 числа
+//определить квадрат числа
+
+
+
+// Console.WriteLine("Введите число a*");
+// string strA = Console.ReadLine();
+// int a = Convert.ToInt32(strA);
+// Console.WriteLine("Введите число b*");
+// string strB = Console.ReadLine();
+// int b = Convert.ToInt32(strA);
+// //int a = 16;
+// //int b = 4;
+// int c = b*b;
+// if (a == c) {
+//     Console.WriteLine ("да");
+// } else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// Программа на вход принемает целое число N , а на выходе показывает все целые числа в промежутке от -N до N
+// 4 => -4,-3.-2,-1,0,1,2,3,4
+
+int num1 = 5;
+int num2 = -5;
+while (num2 != num1 +1) {
+    Console.WriteLine(num2);
+    num2 = num2 +1;
 }
-hjjjjj
-jjjjjj
+//Программа на входе принимае трехзначное число
+//на выходе сумму первой и последней цифр чисел 456 =>10
+
+// //int a = 456;
+// Console.WriteLine("ВВедите число:");
+// string a = Console.ReadLine ();
+// int e = Convert.ToInt32(a);
+// int b = e/100;
+// int c = 456  % 10;
+// int d = b + c;
+// Console.WriteLine(d);
+
+
+
+// int a = 5;
+// Console.WriteLine(a);
+
+// Console.WriteLine("Это число a = 5");
+// Console.WriteLine($"Это число a = {a % 10}");
+
+// Console.WriteLine("Это число a = 5");
+// Console.WriteLine("Это число {a}" + a);
+
+// Console.WriteLine("Это число a");
+/// Console.WriteLine("{1} Это {3} число {2}", 5,4,1);
+}
